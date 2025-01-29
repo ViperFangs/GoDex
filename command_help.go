4d6096248d919dfed1d7075f89370d0224e515b5
ViperFangs <amathrej@asu.edu>
2025-01-28 23:26:47 -0700

    Change commands to getCommands() in command_help.go

--- command_help.go
+++ command_help.go
@@ -5,7 +5,7 @@ import "fmt"
 func commandHelp() error {
 	fmt.Printf("Welcome to the GoDex!\nUsage:\n\n")
 
-	for _, c := range commands {
+	for _, c := range getCommands() {
 		fmt.Printf("%s: %s\n", c.name, c.description)
 	}
 
