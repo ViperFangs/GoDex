4756c7c40c16ab9d7e28f4d6153bfd306a911d6a
ViperFangs <amathrej@asu.edu>
2025-01-29 23:22:15 -0700

    Move function commandMapb to command_map.go

--- command_map.go
+++ command_map.go
@@ -48,3 +48,33 @@ func commandMap(cfg *config) error {
 
 	return nil
 }
+
+func commandMapb(cfg *config) error {
+	resp, err := http.Get(cfg.Previous)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	locationData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	location := Location{}
+	err = json.Unmarshal(locationData, &location)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = json.Unmarshal(locationData, &cfg)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, r := range location.Results {
+		fmt.Println(r.Name)
+	}
+
+	return nil
+}
